870ee6e58080e306ee272eceb1b01569704b72f1
juniorbraz93 <jbraz.est@gmail.com>
2023-02-06 06:20:46 -0300

    chore: first commit

--- App.js
+++ App.js
@@ -1,20 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { 
+  Container,
+  LogoUrL,
+  LogoName,
+  Input,
+  Button,
+  BtnText,
+  Shorten
+ } from './styles';
+
+ import { TouchableWithoutFeedback, Keyboard, ToastAndroid} from 'react-native';
+ import * as Clipboard from 'expo-clipboard';
+//  "https://cutt.ly/api/api.php?key=[API_KEY]&short=$url&name=[CUSTOM_URL_ALIAS]"
 
 export default function App() {
+  const api = '01b6f542ccb023b46089b9ecd5581abf2d188'
+  const [url, setUrl] = useState('')
+  const [name, setName] = useState('')
+  const [shortFinal, setShortFinal] = useState('')
+
+  const short = async () => {
+    Keyboard.dismiss()
+    if(url.includes('https://') || url.includes('http://')) {
+      await fetch(`https://cutt.ly/api/api.php?key=${api}&short=${url}&name=${name}`)
+        .then(async response => {
+          const data = await response.json()
+          if(data.url.status === 3){
+            ToastAndroid.show('Esse nome já esta em uso', ToastAndroid.SHORT)
+            return
+          }
+          if(data.url.status === 2){
+            ToastAndroid.show('url é invalida', ToastAndroid.SHORT)
+            return
+          }
+          
+          setShortFinal(data.url.shortLink)
+          setUrl('')
+          setName('')
+
+        })
+        return
+    }
+
+    ToastAndroid.show('url é invalida', ToastAndroid.SHORT)
+  }
+
+  async function copyUrl() {
+    await Clipboard.setStringAsync(shortFinal)
+    ToastAndroid.show('url copiada com sucesso!', ToastAndroid.SHORT);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <Container>
+        <LogoUrL>
+          url
+          <LogoName>JBraz</LogoName>
+        </LogoUrL>
+        <Input
+          placeholder="Digite a URL..."
+          onChangeText={ (text) => setUrl(text) }
+          value={url}
+        />
+
+        <Input
+          placeholder="Nome personalizado."
+          onChangeText={ (text) => setName(text) }
+          value={name}
+        />
+
+        <Button
+          onPress={ () => short() }
+        >
+          <BtnText>
+            Encurtar
+          </BtnText>
+        </Button>
+        <TouchableWithoutFeedback onPress={ shortFinal ? copyUrl : () => {} }>
+          <Shorten>
+            {shortFinal}
+          </Shorten>
+        </TouchableWithoutFeedback>
+
+      </Container>
+    </TouchableWithoutFeedback>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.js
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const Container = styled.View`
+  flex:1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LogoUrL = styled.Text`
+  color: #21243D
+  font-weight: bold;
+  font-size: 40px;
+  margin-bottom: 20px;
+`;
+
+export const LogoName = styled.Text`
+  color: #02FE75
+`;
+
+export const Input = styled.TextInput`
+  height: 50px;
+  width: 80%;
+  border-color: #21243D;
+  border-width: 1px;
+  border-radius: 5px;
+  padding: 10px;
+  background-color: #FAFAFA;
+  margin-bottom: 20px;
+  font-size: 20px;
+`;
+
+export const Button = styled.TouchableOpacity`
+  background-color: #FF7C7C;
+  border-radius: 20px;
+  height: 40px;
+  width: 80%;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const BtnText = styled.Text`
+  color: #FFF;
+  font-size: 20px;
+`;
+
+export const Shorten = styled. Text`
+  height: 40px;
+  width: 85%;
+  margin-top: 20px;
+  font-size: 20px;
+  text-align: center;
+`;
